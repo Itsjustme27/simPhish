bbfe663b10ad9f91b1d377930e783046ed07a907
Itsjustme27 <reachprayush@gmail.com>
2025-05-16 21:31:35 +0545

    basic victim functionality completed

--- phish/public/index.php
+++ phish/public/index.php
@@ -6,13 +6,12 @@
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
-    <div class="globalContainer">
+    <div class="globalContainer main-content">
         <div class="fb_content d-flex justify-content-center align-items-center">
             <div>
                 <div class="_8esj">
